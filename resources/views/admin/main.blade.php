1f0685a2f48098fd4b30add845e71645f9376a3c
anxysz <lucknurfitriana17@gmail.com>
2023-07-24 03:44:08 +0700

    -admhistory

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -148,7 +148,90 @@
     <script src="{{ asset('style/vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
     <script src="{{ asset('style/assets/js/main.js')}} "></script>
     <script src="{{ asset('style/vendors/chart.js/dist/Chart.bundle.min.js') }}"></script>
-    <script src="{{ asset('js/init-scripts/chart-js/chartjs-init.js') }}"></script>
+    <script src="{{ asset('style/assets/js/init-scripts/chart-js/chartjs-init.js') }}"></script>
+    <script>
+        var ctx = document.getElementById( "sales-chart" );
+    ctx.height = 70;
+    var myChart = new Chart( ctx, {
+        type: 'line',
+        data: {
+            labels: [ "2010", "2011", "2012", "2013", "2014", "2015", "2016" ],
+            type: 'line',
+            defaultFontFamily: 'Montserrat',
+            datasets: [ {
+                label: "Foods",
+                data: [ 0, 30, 10, 120, 50, 63, 10 ],
+                backgroundColor: 'transparent',
+                borderColor: 'rgba(220,53,69,0.75)',
+                borderWidth: 3,
+                pointStyle: 'circle',
+                pointRadius: 5,
+                pointBorderColor: 'transparent',
+                pointBackgroundColor: 'rgba(220,53,69,0.75)',
+                    }, {
+                label: "Electronics",
+                data: [ 0, 50, 40, 80, 40, 79, 120 ],
+                backgroundColor: 'transparent',
+                borderColor: 'rgba(40,167,69,0.75)',
+                borderWidth: 3,
+                pointStyle: 'circle',
+                pointRadius: 5,
+                pointBorderColor: 'transparent',
+                pointBackgroundColor: 'rgba(40,167,69,0.75)',
+                    } ]
+        },
+        options: {
+            responsive: true,
+
+            tooltips: {
+                mode: 'index',
+                titleFontSize: 12,
+                titleFontColor: '#000',
+                bodyFontColor: '#000',
+                backgroundColor: '#fff',
+                titleFontFamily: 'Montserrat',
+                bodyFontFamily: 'Montserrat',
+                cornerRadius: 3,
+                intersect: false,
+            },
+            legend: {
+                display: false,
+                labels: {
+                    usePointStyle: true,
+                    fontFamily: 'Montserrat',
+                },
+            },
+            scales: {
+                xAxes: [ {
+                    display: true,
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    scaleLabel: {
+                        display: false,
+                        labelString: 'Month'
+                    }
+                        } ],
+                yAxes: [ {
+                    display: true,
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value'
+                    }
+                        } ]
+            },
+            title: {
+                display: false,
+                text: 'Normal Legend'
+            }
+        }
+    } );
+    </script>
     <script>
         var rupiah = document.getElementById('rupiah');
         // rupiah.addEventListener('input', function(e) {
