05e90bf1f871dc14a25e49ef7f627c665eb6844c
Febriadi112 <febriadu6@gmail.com>
2023-07-23 19:53:39 +0700

    checkout

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,7 +7,7 @@ use GuzzleHttp\Client;
 
 class ApiController extends Controller
 {
-    private $baseUrl = "https://bca5-103-162-237-197.ngrok-free.app/api/v1/";
+    private $baseUrl = "https://4ff5-103-162-237-197.ngrok-free.app/api/v1/";
 
     public function getDataPenginap()
     {
